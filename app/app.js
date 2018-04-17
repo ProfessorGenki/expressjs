2ebd81b79a552124f9b13c86cc96f7721494565c
ProfessorGenki <getyowalkon@gmail.com>
2018-04-17 12:14:48 +0200

    gay
    yyy

--- app/app.js
+++ app/app.js
@@ -1,20 +1,18 @@
-var express = require('express')
+var express = require ('express');
 var app = express();
-
-app.get('/', function(req, res) {
-    res.send('<h1> Big Gay </h1>');
+app.get('/', function(request,response){
+response.send('<h1> Hassan 22 år <h1>');
 });
-
-var server = app.listen(3000, function() {
-    console.log('Linstening on port 3000')
-});
+var server = app.listen(3000,function() {
+console.log('lytter til port 3000');
+})
 
 //var http = require('http');
 
 //var myServer = http.createServer(function(request, response)
 //{
 //response.writeHead(200, {"Content-Type" : "text/plain"});
-//response.write('<h1> Kasper </h1>');
+//response.write('<h1> Gayyy </h1>');
 //response.end();
 //});
 
